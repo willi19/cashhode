16b6329cf64d8b1f7af388930bd4e7f5248144bb
이준영 <rickmccoy3141@gmail.com>
2021-02-21 15:50:38 +0900

    Added interest analysis

--- analysis.py
+++ analysis.py
@@ -29,10 +29,13 @@ if __name__ == '__main__':
         rng = np.random.default_rng()
         interests = []
         for _ in range(100000):
-            tag_set1 = set(tags_list[rng.choice(num_photos)])
-            tag_set2 = set(tags_list[rng.choice(num_photos)])
+            indices = rng.choice(num_photos, size=2, replace=False)
+            tag_set1 = set(tags_list[indices[0]])
+            tag_set2 = set(tags_list[indices[1]])
             interest = min(len(tag_set1.difference(tag_set2)), len(tag_set2.difference(tag_set1)), len(tag_set1.intersection(tag_set2)))
             interests.append(interest)
+
+        print(f"Nonzero interest: {sum(i > 0 for i in interests)} out of 100,000")
 
         plt.subplot(1, 2, 2)
         plt.title('Randomly selected Interest factors')
