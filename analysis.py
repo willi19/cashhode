5e3839f29fad966e58bd726f0471ad223b36abdb
이준영 <rickmccoy3141@gmail.com>
2021-02-21 16:15:25 +0900

    Analyzing nonzero pairings of every dataset

--- analysis.py
+++ analysis.py
@@ -3,6 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 from load import load
+from ga import get_interest
 
 FILENAMES = [
     'a.txt',
@@ -18,29 +19,37 @@ if __name__ == '__main__':
         num_photos, num_V, num_H, orients, num_tags, tags_list = load(filename)
         print(f'{filename}: {num_photos} photos, {num_V} vertical, {num_H} horizontal, {num_tags} total tags')
         num_tags = [len(tags) for tags in tags_list]
-        plt.figure(figsize=(12, 4))
-        plt.title(f'Dataset {filename}')
-        plt.subplot(1, 2, 1)
-        plt.title('Tags per photo')
-        plt.hist(num_tags, bins=100)
-        plt.xlabel('# of tags')
-        plt.ylabel('# of photos')
+        # plt.figure(figsize=(12, 4))
+        # plt.title(f'Dataset {filename}')
+        # plt.subplot(1, 2, 1)
+        # plt.title('Tags per photo')
+        # plt.hist(num_tags, bins=100)
+        # plt.xlabel('# of tags')
+        # plt.ylabel('# of photos')
 
-        rng = np.random.default_rng()
-        interests = []
-        for _ in range(100000):
-            indices = rng.choice(num_photos, size=2, replace=False)
-            tag_set1 = set(tags_list[indices[0]])
-            tag_set2 = set(tags_list[indices[1]])
-            interest = min(len(tag_set1.difference(tag_set2)), len(tag_set2.difference(tag_set1)), len(tag_set1.intersection(tag_set2)))
-            interests.append(interest)
+        # rng = np.random.default_rng()
+        # interests = []
+        nonzero = 0
+        tag_sets = [set(tag_list) for tag_list in tags_list]
+        for i in range(num_photos):
+            for j in range(i + 1, num_photos):
+                interest = get_interest(tag_sets[i], tag_sets[j])
+                if interest > 0:
+                    nonzero += 1
+        print(f'Nonzero interest: {nonzero} out of {num_photos * (num_photos - 1) // 2}')
+        # for _ in range(100000):
+        #     indices = rng.choice(num_photos, size=2, replace=False)
+        #     tag_set1 = set(tags_list[indices[0]])
+        #     tag_set2 = set(tags_list[indices[1]])
+        #     interest = min(len(tag_set1.difference(tag_set2)), len(tag_set2.difference(tag_set1)), len(tag_set1.intersection(tag_set2)))
+        #     interests.append(interest)
 
-        print(f"Nonzero interest: {sum(i > 0 for i in interests)} out of 100,000")
+        # print(f"Nonzero interest: {sum(i > 0 for i in interests)} out of 100,000")
 
-        plt.subplot(1, 2, 2)
-        plt.title('Randomly selected Interest factors')
-        plt.hist(interests, bins=100)
-        plt.xlabel('Interest factor')
-        plt.ylabel('# of pairs')
-        plt.savefig(os.path.join('figures', filename.split('.')[0] + '.png'))
-        plt.close()
+        # plt.subplot(1, 2, 2)
+        # plt.title('Randomly selected Interest factors')
+        # plt.hist(interests, bins=100)
+        # plt.xlabel('Interest factor')
+        # plt.ylabel('# of pairs')
+        # plt.savefig(os.path.join('figures', filename.split('.')[0] + '.png'))
+        # plt.close()
